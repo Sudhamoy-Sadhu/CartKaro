ea5830601d503d275bd93ca9921d8a93ba13c263
Sudhamoy-Sadhu <101017873+Sudhamoy-Sadhu@users.noreply.github.com>
2024-03-11 21:53:08 +0530

    Project er Gar mere gache

--- CartKaro/resources/views/Dashboard/addmember.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('Dashboard.main');
-@push('title')
-    <title>Dashboard - Add Members</title> {{-- Enter Your Page Title Here--}}
-@endpush
-@section('content')
-    {{--Enter Your Code Here--}}
-    {{-- Route of your page is /addmembers --}}
-    I am Anupama
-@endsection
--- CartKaro/resources/views/Dashboard/analysis.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('Dashboard.main');
-@push('title')
-    <title>Dashboard - Analysis</title> {{-- Enter Your Page Title Here--}}
-@endpush
-@section('content')
-    {{--Enter Your Code Here--}}
-    I am Subhrajit 
-@endsection
