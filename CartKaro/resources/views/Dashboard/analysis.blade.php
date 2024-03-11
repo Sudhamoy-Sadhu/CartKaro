4bcb1df0d75158ebf9539bbb097f3c9a35f56e88
Sudhamoy sadhu <sudhamoy.sadhu2004@gmail.com>
2024-03-11 22:11:58 +0530

    Project Notun

---
+++ CartKaro/resources/views/Dashboard/analysis.blade.php
@@ -0,0 +1,8 @@
+@extends('Dashboard.main');
+@push('title')
+    <title>Dashboard - Analysis</title> {{-- Enter Your Page Title Here--}}
+@endpush
+@section('content')
+    {{--Enter Your Code Here--}}
+    I am Subhrajit 
+@endsection
---
+++ CartKaro/resources/views/Dashboard/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('Dashboard.main');
+@push('title')
+    <title>Dashboard - Edit</title> {{-- Enter Your Page Title Here--}}
+@endpush
+@section('content')
+    {{--Enter Your Code Here--}}
+    {{-- Route of your page is /edit --}}
+    I am Abhisikta 
+@endsection
