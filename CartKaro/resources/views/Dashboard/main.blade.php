0e6925cdd04a85c60aeb587e21ceb1321cf0a49d
Sudhamoy sadhu <sudhamoy.sadhu2004@gmail.com>
2024-03-13 01:39:34 +0530

    Anupama's task done

--- CartKaro/resources/views/Dashboard/main.blade.php
+++ CartKaro/resources/views/Dashboard/main.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ URL::asset('css/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/addmember.css') }}">
     @stack('title')
     <style>
 
